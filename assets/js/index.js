d62b2e2ca75067aca0a8c1168962a049c0ecccc3
nttminh <ng.minh@hotmail.com>
2022-04-30 18:29:14 -0400

    refactor: 💡 give descriptive names to variables

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,49 +1,48 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
-// header script dark-light mode
-const moon = $('.far.fa-moon');
-const sun = $('.far.fa-sun');
-const day = $('.day.ms-3');
-var isLightTheme = true;
 
 // html elements
-const navbarToggler = $('.navbar-toggler');
 const header = $('#site-header');
-const headerRespondsive = $('.navbar-toggler');
+const navbarTogglerButton = $('.navbar-toggler');
+const navbarToggler = $('.navbar-toggler');
 var isActive = false;
 
 // handle search event
 navbarToggler.onclick = function () {
     isActive = !isActive;
     header.classList.toggle('active', isActive)
-    headerRespondsive.classList.toggle('close', isActive);
+    navbarTogglerButton.classList.toggle('close', isActive);
 }
 // end of searching function 
 
 // scroll to top event
 var goTopButton = $('.goTop');
 var carouselSection = $('.carousel-section');
-var sticky = carouselSection.offsetTop;
-window.onscroll = function () { showingHeader() };
-function showingHeader() {
-    var isScroll = window.pageYOffset > sticky ? true : false;
-    header.classList.toggle('scroll', isScroll);
-    goTopButton.style.display = (isScroll) ? 'block' : 'none';
+var triggerPoint = carouselSection.offsetTop;
+var showHeader = function () {
+    var pageDidScroll = window.pageYOffset > triggerPoint;
+    header.classList.toggle('scroll', pageDidScroll);
+    goTopButton.style.display = (pageDidScroll) ? 'block' : 'none';
 }
+window.onscroll = function () { showHeader() };
 // done scroll to top even
 
-// start function to turn on/off dark mode 
-day.onclick = function () {
+// Dark mode toggler
+const moonIcon = $('.far.fa-moon');
+const sunIcon = $('.far.fa-sun');
+const dayBtn = $('.day.ms-3');
+var isLightTheme = true;
+dayBtn.onclick = function () {
     isLightTheme = !isLightTheme;
     if (isLightTheme) {
-        sun.style.display = 'none';
-        moon.style.display = 'block';
+        sunIcon.style.display = 'none';
+        moonIcon.style.display = 'block';
         $('html').setAttribute('data-theme', 'light')
     } else {
-        sun.style.display = 'block';
-        moon.style.display = 'none';
+        sunIcon.style.display = 'block';
+        moonIcon.style.display = 'none';
         $('html').setAttribute('data-theme', 'dark')
     }
 }
-//end function on/off dark mode
+// End of dark mode toggler
